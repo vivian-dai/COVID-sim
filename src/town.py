c618a219d6e0c8f9e1bfc8180820669050ec3124
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-03-13 16:58:38 -0500

    created building and person

--- src/town.py
+++ src/town.py
@@ -1,2 +1,32 @@
-# town.py
-# classes for things that exist in the town (eg. Buildings, Humans)
+"""
+town.py
+Contains a bunch of classes that are used throughout the town
+"""
+
+class Building:
+    """
+    Building class template
+    """
+    def __init__(self, x, y):
+        """ Constructor for the Building class
+
+        Args:
+            x (int): the x position of the Building
+            y (int): the y position of the Building
+        """
+        self.x = x
+        self.y = y
+
+class Person:
+    """
+    Person class template
+    """
+    def __init__(self, x, y):
+        """Constructor for the Person class
+
+        Args:
+            x (int): x position of the Person
+            y (int): y position of the Person
+        """
+        self.x = x
+        self.y = y
