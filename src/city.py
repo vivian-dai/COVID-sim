3cde38eef87c32783e4d9505836b11c88d343ca5
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-03-13 21:07:35 -0500

    ....pygame display

--- src/city.py
+++ src/city.py
@@ -11,15 +11,18 @@ class City:
     A template for a City
     """
     def __init__(self):
-        buildings = []
-        people = []
+        """
+        ...constructor for a City
+        """
+        self.buildings = []
+        self.people = []
         building_types = []
         for i in range(len(town.BUILDINGS)):
             building_types.append(i)
         random.shuffle(building_types)
         for i in range(5):
             for j in range(5):
-                buildings.append(town.Building(i*town.Building.BUILDING_SIZE,
+                self.buildings.append(town.Building(i*town.Building.BUILDING_SIZE,
                 j*town.Building.BUILDING_SIZE, building_types[(i*j)%len(building_types)], True))
 
         sexes = ['M', 'F']
@@ -32,41 +35,41 @@ class City:
                 age = random.randint(0, 100)
                 if age >= 65:
                     possible_buildings = []
-                    for j in buildings:
+                    for j in self.buildings:
                         if town.BUILDINGS[int(j.building_type_index)] == "Elderly":
                             possible_buildings.append(j)
-                    people.append(town.Person(random.choice(possible_buildings), 
+                    self.people.append(town.Person(random.choice(possible_buildings), 
                     age, occupation, sex, random.choice(tf), random.choice(tf)))
                 else:
                     possible_buildings = []
-                    for j in buildings:
+                    for j in self.buildings:
                         if town.BUILDINGS[int(j.building_type_index)] == "Residential":
                             possible_buildings.append(j)
-                    people.append(town.Person(random.choice(possible_buildings), 
+                    self.people.append(town.Person(random.choice(possible_buildings), 
                     age, occupation, sex, random.choice(tf), random.choice(tf)))
             elif town.PROFESSIONS[occupation] == "Doctor":
                 age = random.randint(30, 65)
                 possible_buildings = []
-                for j in buildings:
+                for j in self.buildings:
                     if town.BUILDINGS[int(j.building_type_index)] == "Hospital":
                         possible_buildings.append(j)
-                people.append(town.Person(random.choice(possible_buildings), 
+                self.people.append(town.Person(random.choice(possible_buildings), 
                 age, occupation, sex, random.choice(tf), random.choice(tf)))
             elif town.PROFESSIONS[occupation] == "Business owner":
                 age = random.randint(30, 65)
                 possible_jobs = ["Retail", "Food", "Residential", "Entertainment", 
                 "Police station", "9-5 job", "School"]
                 possible_buildings = []
-                for j in buildings:
+                for j in self.buildings:
                     if town.BUILDINGS[int(j.building_type_index)] in possible_jobs:
                         possible_buildings.append(j)
-                people.append(town.Person(random.choice(possible_buildings), 
+                self.people.append(town.Person(random.choice(possible_buildings), 
                 age, occupation, sex, random.choice(tf), random.choice(tf)))
             elif town.PROFESSIONS[occupation] == "From home":
                 age = random.randint(30, 65)
                 possible_buildings = []
-                for j in buildings:
+                for j in self.buildings:
                     if town.BUILDINGS[int(j.building_type_index)] == "Residential":
                         possible_buildings.append(j)
-                people.append(town.Person(random.choice(possible_buildings), 
+                self.people.append(town.Person(random.choice(possible_buildings), 
                 age, occupation, sex, random.choice(tf), random.choice(tf)))
--- src/main.py
+++ src/main.py
@@ -5,5 +5,17 @@ import pygame
 main.py
 The main program (run this!!!)
 """
-# print(town.Building.BUILDING_SIZE)
+pygame.init()
 sim = city.City()
+screen = pygame.display.set_mode((town.Building.BUILDING_SIZE*5, town.Building.BUILDING_SIZE*5))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+    for building in sim.buildings:
+        x = building.x
+        y = building.y
+        size = town.Building.BUILDING_SIZE
+        pygame.draw.rect(screen, (255, 0, 0), (x, y, size, size), 1)
+
+    pygame.display.flip()
